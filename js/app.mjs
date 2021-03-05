59035f994c0be8e7400a7debe745307d901c5118
Tony Grimes <acidtone@tonygrimes.com>
2021-03-04 23:11:49 -0700

    added catch on 404, updated endpoint

--- js/app.mjs
+++ js/app.mjs
@@ -1,7 +1,12 @@
-fetch('https://json-express.herokuapp.com/api/definitions')
+fetch('https://json-express.herokuapp.com/api/v0/definitions')
   .then(function(response){
+    if (!response.ok) throw new Error('not 200');
+    
     return response.json();
   })
   .then(function(data){
     console.log(data);
+  })
+  .catch(function(err){
+    console.log(err);
   });
